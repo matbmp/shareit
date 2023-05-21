a708e881e2f1dda0b7b34c66f5d9f75b2aa3b72c
matbmp <96355280+matbmp@users.noreply.github.com>
2023-05-21 12:24:55 +0200

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+
+<script>
+    import {Button} from 'flowbite-svelte'
+    import Icon from 'virtual:icons/mdi/rocket-launch-outline'
+</script>
+
+<div class='p-4 text-lg'>
+    <h1 class='text-2xl'>Welcome to SvelteKit</h1>
+    This template includes:
+    <ul>
+        <li class='my-2'><button class='rounded-xl bg-blue-400 text-white p-2 px-4'>Tailwind</button></li>
+        <li class='my-2'><Button color='red'>Flowbite</Button></li>
+        <li>unplugin-icons with Material Design Icon pack <Icon class='inline-block w-10 h-10'/> </li>
+    </ul>
+    
+</div>
